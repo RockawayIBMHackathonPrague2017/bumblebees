e52de78ed523c85e7995c6f9bab388507e97c65d
jakub lecbych <lecbychjakub@gmail..com>
2017-12-02 08:09:04 +0100

    db connection + api for filtering results

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from cloudant import Cloudant
+from cloudant.query import Query
 from flask import Flask, render_template, request, jsonify
 import atexit
 import cf_deployment_tracker
@@ -10,9 +11,10 @@ cf_deployment_tracker.track()
 
 app = Flask(__name__)
 
-db_name = 'mydb'
+db_name = 'products'
 client = None
 db = None
+project_dir = os.path.dirname(os.path.realpath(__file__))
 
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.getenv('VCAP_SERVICES'))
@@ -24,33 +26,71 @@ if 'VCAP_SERVICES' in os.environ:
         url = 'https://' + creds['host']
         client = Cloudant(user, password, url=url, connect=True)
         db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
+elif os.path.isfile(os.path.join(project_dir, 'vcap-local.json')):
+    with open(os.path.join(project_dir, 'vcap-local.json')) as f:
         vcap = json.load(f)
         print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+        creds = vcap['cloudantNoSQLDB'][0]['credentials']
         user = creds['username']
         password = creds['password']
         url = 'https://' + creds['host']
         client = Cloudant(user, password, url=url, connect=True)
         db = client.create_database(db_name, throw_on_exists=False)
 
+
+def find_relevant(selector):
+    findData = {
+        "selector": selector,
+        "fields": ["_id", "ITEM_ID" "PRODUCTNAME", "DESCRIPTION", "CATEGORYTEXT", "PRICE_VAT", "IMGURL", "URL",
+                   "PARAMS"],
+    }
+
+    query = Query(db, selector=findData["selector"], fields=findData["fields"])
+    resp = query(skip=0, r=1)
+    return resp["docs"]
+
+
+def get_all(limit):
+    findData = {
+        "fields": ["_id", "ITEM_ID" "PRODUCTNAME", "DESCRIPTION", "CATEGORYTEXT", "PRICE_VAT", "IMGURL", "URL",
+                   "PARAMS"],
+    }
+
+    query = Query(db, selector=findData["selector"], fields=findData["fields"])
+    resp = query(skip=0, limit=limit, r=1)
+    return resp["docs"]
+
+
 # On Bluemix, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
 
+
 @app.route('/')
 def home():
+    # return render_template(os.path.join(project_dir, 'static\index.html'))
     return render_template('index.html')
 
-@app.route('/api', methods=['GET'])
-def get_visitor():
-    return jsonify({'hello': 'world'})
+
+@app.route('/all', methods=['GET'])
+def get_20_results():
+    return get_all(20)
+
+
+@app.route('/filtered/<int:product_id>', methods=['GET'])
+def get_50_results(product_id):
+    product = find_relevant({"ITEM_ID": product_id})
+    if product.length == 1:
+        return find_relevant({"CATEGORYTEXT": product[0]["CATEGORYTEXT"]})
+    else:
+        return []
+
 
 @atexit.register
 def shutdown():
     if client:
         client.disconnect()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
