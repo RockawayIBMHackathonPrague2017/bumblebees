d9b99c554ada971df191c9ed2ecff32e0330f872
jakub lecbych <lecbychjakub@gmail..com>
2017-12-02 16:32:59 +0100

    set encoding to utf8

--- distance.py
+++ distance.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
 from math import *
 from decimal import Decimal
 from scipy.spatial import distance
